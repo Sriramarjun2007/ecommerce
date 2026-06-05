aa6bb330fef194262c774477f1b62eac76d61a60
Sriram Arjun <sriramarjun2007@gmail.com>
2026-06-05 20:19:41 +0530

    query  update

--- index.js
+++ index.js
@@ -32,3 +32,24 @@ function addtocart() {
 
     alert("Added to cart!");
 }
+document.addEventListener("DOMContentLoaded", function () {
+
+    let questions = document.querySelectorAll(".faq-question");
+
+    questions.forEach(function(question) {
+
+        question.addEventListener("click", function() {
+
+            let answer = question.nextElementSibling;
+
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+
+        });
+
+    });
+
+});
